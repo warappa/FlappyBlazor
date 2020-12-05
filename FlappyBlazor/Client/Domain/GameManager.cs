fa48d1817f08010ee725d89ea911af103d457dc2
warappa <spoiltnr1@gmx.at>
2020-12-05 01:16:04 +0100

    Moving pipes

--- FlappyBlazor/Client/Domain/GameManager.cs
+++ FlappyBlazor/Client/Domain/GameManager.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -15,8 +13,6 @@ namespace FlappyBlazor.Client.Domain
 
         public bool Jumped { get; private set; }
 
-        private double deltaSum = 0;
-
         public void Move(double delta)
         {
             if (Jumped)
@@ -29,17 +25,69 @@ namespace FlappyBlazor.Client.Domain
             Top += (int)Math.Floor(Velocity);
 
             Top = Math.Min(Math.Max(Top, 0), 216 - 12);
+            if (Top == 216 - 12)
+            {
+                Velocity = 0;
+            }
         }
 
         public void Jump()
         {
             Jumped = true;
         }
+
+        public void Reset()
+        {
+            Top = 0;
+        }
+    }
+
+    public class Pipe
+    {
+        public int Left { get; set; }
+        public int Top { get; set; }
+    }
+
+    public class Pipes
+    {
+        public Pipes()
+        {
+            PipeBottom = new Pipe();
+            PipeTop = new Pipe();
+        }
+
+        public Pipe PipeTop { get; set; }
+        public Pipe PipeBottom { get; set; }
+
+        public double Velocity { get; set; } = 1.0;
+
+        public void Move(double delta)
+        {
+            //Console.WriteLine($"Velocity {Gravity} {Velocity} {delta}");
+            PipeTop.Left -= (int)Math.Floor(Velocity * delta);
+            PipeBottom.Left -= (int)Math.Floor(Velocity * delta);
+
+            if (PipeTop.Left < -30)
+            {
+                Reset();
+            }
+        }
+
+        public void Reset()
+        {
+            PipeTop.Left = 150;
+            PipeBottom.Left = 150;
+            var random = 128 - 40 + new Random().Next(-44, 44);
+            PipeTop.Top = random - 25;
+            PipeBottom.Top = random + 25;
+        }
     }
 
     public class GameManager
     {
         public Bird Bird { get; set; }
+        public Pipes Pipes { get; set; }
+
         public bool IsRunning { get; private set; }
 
         private CancellationTokenSource loopCancellationToken;
@@ -56,8 +104,12 @@ namespace FlappyBlazor.Client.Domain
             }
 
             Console.WriteLine("Start Game");
+
             Bird = new Bird();
-            Bird.Top = 0;
+            Bird.Reset();
+
+            Pipes = new Pipes();
+            Pipes.Reset();
 
             IsRunning = true;
 
@@ -80,6 +132,7 @@ namespace FlappyBlazor.Client.Domain
                 //Console.WriteLine($"Loop {delta} {deltaMs} {stopwatch.ElapsedMilliseconds}");
 
                 Bird.Move(delta);
+                Pipes.Move(delta);
 
                 lastTicks = stopwatch.ElapsedMilliseconds;
 
